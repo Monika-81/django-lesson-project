2d7c82277be1df7d1f30aade09e0d0a6b721571b
Monika Jakobsson <nimrin81@gmail.com>
2022-05-25 23:20:56 +0000

    Inital commit

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Item(models.Model):
+    # prevents items created without a name
+    name = models.CharField(max_length=50, null=False, blank=False)
+    done = models.BooleanField(null=False, blank=False, default=False)
+
+    def __str__(self):
+        return self.name
+
