938be876402958d3a96d7b7af75806dafbaf3ade
Monika Jakobsson <nimrin81@gmail.com>
2022-05-28 19:09:51 +0000

    Add import os

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -1,3 +1,5 @@
+import os
+
 """
 Django settings for django_todo project.
 
@@ -9,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-import os
+
 from pathlib import Path
 import dj_database_url
 
